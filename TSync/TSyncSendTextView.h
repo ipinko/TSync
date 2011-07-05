73a21fd3df88dfe97394e59cff9fa9cce7cedb6b
ipinko <ipinko@21cn.com>
2011-07-06 02:25:50 +0800

    TextView焦点,字数倒数,发送按钮是否可用..loading等…

---
+++ TSync/TSyncSendTextView.h
@@ -0,0 +1,13 @@
+//
+//  TSyncSendTextView.h
+//  TSync
+//
+//  Created by Ljw on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSyncSendTextView : UITextView
+
+@end
---
+++ TSync/TSyncSendTextView.m
@@ -0,0 +1,28 @@
+//
+//  TSyncSendTextView.m
+//  TSync
+//
+//  Created by Ljw on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TSyncSendTextView.h"
+
+@implementation TSyncSendTextView
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+
+}
+@end
